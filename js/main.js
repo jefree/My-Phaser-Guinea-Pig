e00e659cf47e8763ed17738c36bbf54f2a8d2024
jefree <jgarzon920429@gmail.com>
2015-01-06 22:39:38 -0500

    refactor Razz

--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
 var game = new Phaser.Game(780, 455, Phaser.CANVAS, 'canvas', {preload: preload, create: create, update: update});
+var razz = new Razz(game);
 
 function preload() {
-  game.load.atlasJSONArray('razz', 'assets/img/chars/razz.png', 'assets/img/chars/razz.json');
   game.load.atlasJSONArray('maptiles', 'assets/img/tiles/maptiles.png', 'assets/img/tiles/maptiles.json')
   game.load.tilemap('map', 'assets/maps/map.json', null, Phaser.Tilemap.TILED_JSON);
+
+  razz.preload();
 }
-
-var razz;
-var map;
-var layer;
-var cursors;
-var cube;
-
-var jump;
 
 function create() {
 
@@ -21,85 +15,16 @@ function create() {
 
   map.setCollisionBetween(1, 5);
 
-  layer = map.createLayer('Capa de Patrones 1');
+  var layer = map.createLayer('Capa de Patrones 1');
   layer.resizeWorld();
 
-  razz = game.add.sprite(0, 455-(65+61), 'razz');
-  game.physics.enable(razz);
+  razz.create(layer);
 
-  razz.body.setSize(29, 61, 25, 0);
-  razz.body.collideWorldBounds = true;
-
-  razz.animations.add('right', ['right1.png', 'right2.png', 'right3.png', 'right4.png'], 8);
-  razz.animations.add('left', ['left1.png', 'left2.png', 'left3.png', 'left4.png'], 8);
-
-  razz.animations.add('jright', ['jright1.png', 'jright2.png', 'jright3.png'], 3);
-  razz.animations.add('jleft', ['jleft1.png', 'jleft2.png', 'jleft3.png'], 3);
-
-  razz.animations.add('pright', ['pright1.png', 'pright2.png', 'pright3.png', 'pright4.png'], 8);
-  razz.animations.add('pleft', ['pleft1.png', 'pleft2.png', 'pleft3.png', 'pleft4.png'], 8);
-
-  game.camera.follow(razz);
+  game.camera.follow(razz.razz);
   game.physics.arcade.gravity.y = 300;
-
-  cursors = game.input.keyboard.createCursorKeys();
-
-  cursors.up.onDown.add(function() {
-
-    if (razz.body.onFloor()){
-      razz.body.velocity.y = -200;
-      jump = true;
-    }
-  });
-  
+ 
 }
 
 function update() {
-  game.physics.arcade.collide(razz, layer);
-  game.physics.arcade.collide(cube, layer);
-
-  if (razz.body.onFloor()) {
-    razz.body.velocity.x = 0
-  }
-
-  if (cursors.right.isDown) {
-    razz.body.velocity.x = 50;
-
-    //set collision bounds
-    razz.body.setSize(28, 61, 25, 0);
-
-    if (razz.body.onFloor()) {
-      razz.animations.play('right');
-    }
-
-  } 
-  else if (cursors.left.isDown) {
-
-    razz.body.velocity.x = -50;
-
-    //set collision bounds
-    razz.body.setSize(28, 61, 12, 0);
-
-    if (razz.body.onFloor()) {
-      razz.animations.play('left');
-    }
-
-  }
-  else {
-    if (razz.animations.getAnimation('right').isPlaying) {
-      razz.animations.stop('right');
-    }
-  }
-
-  if (jump) {
-    
-    if (cursors.left.isDown) {
-      razz.animations.play('jleft');
-    }
-    else if (cursors.right.isDown) {
-      razz.animations.play('jright');
-    }
-
-    jump = false;
-  }
+  razz.update();
 }
---
+++ js/razz.js
@@ -0,0 +1,100 @@
+var Razz = function(game) {
+  this.game = game;
+  
+  this.BODY_WIDTH = 28;
+  this.BODY_HEIGHT = 61;
+
+  this.LOOK_RIGHT_OFFSET_X = 25;
+  this.LOOK_LEFT_OFFSET_X = 12;
+};
+
+Razz.prototype.on_up_down = function() {
+
+  if (this.razz.body.onFloor()){
+    this.razz.body.velocity.y = -200;
+    this.jump = true;
+  }
+}
+
+Razz.prototype.create_cursors = function() {
+  this.cursors = this.game.input.keyboard.createCursorKeys();
+  this.cursors.up.onDown.add(this.on_up_down, this);
+};
+
+Razz.prototype.preload = function() {
+  this.game.load.atlasJSONArray('razz', 'assets/img/chars/razz.png', 'assets/img/chars/razz.json');
+};
+
+Razz.prototype.create = function(level) {
+
+  this.level = level;
+
+  this.razz = game.add.sprite(0, 0, 'razz');
+  this.razz.position.y = this.game.height - (this.level.map.tileHeight + this.razz.height);
+
+  this.game.physics.enable(this.razz);
+
+  this.razz.body.setSize(29, 61, 25, 0);
+  this.razz.body.collideWorldBounds = true;
+
+  this.razz.animations.add('right', ['right1.png', 'right2.png', 'right3.png', 'right4.png'], 8);
+  this.razz.animations.add('left', ['left1.png', 'left2.png', 'left3.png', 'left4.png'], 8);
+
+  this.razz.animations.add('jright', ['jright1.png', 'jright2.png', 'jright3.png'], 3);
+  this.razz.animations.add('jleft', ['jleft1.png', 'jleft2.png', 'jleft3.png'], 3);
+
+  this.razz.animations.add('pright', ['pright1.png', 'pright2.png', 'pright3.png', 'pright4.png'], 8);
+  this.razz.animations.add('pleft', ['pleft1.png', 'pleft2.png', 'pleft3.png', 'pleft4.png'], 8);
+
+  this.create_cursors();
+
+};
+
+Razz.prototype.update = function() {
+  this.game.physics.arcade.collide(this.razz, this.level);
+
+  if (this.razz.body.onFloor()) {
+    this.razz.body.velocity.x = 0
+  }
+
+  if (this.cursors.right.isDown) {
+    this.razz.body.velocity.x = 50;
+
+    //set collision bounds
+    this.razz.body.setSize(28, 61, 25, 0);
+
+    if (this.razz.body.onFloor()) {
+      this.razz.animations.play('right');
+    }
+
+  } 
+  else if (this.cursors.left.isDown) {
+
+    this.razz.body.velocity.x = -50;
+
+    //set collision bounds
+    this.razz.body.setSize(28, 61, 12, 0);
+
+    if (this.razz.body.onFloor()) {
+      this.razz.animations.play('left');
+    }
+
+  }
+  else {
+    if (this.razz.animations.getAnimation('right').isPlaying) {
+      this.razz.animations.stop('right');
+    }
+  }
+
+  if (this.jump) {
+    
+    if (this.cursors.left.isDown) {
+      this.razz.animations.play('jleft');
+    }
+    else if (this.cursors.right.isDown) {
+      this.razz.animations.play('jright');
+    }
+
+    this.jump = false;
+  }
+};
